7e2597cdbc8af63e364c4978e49f0cf7bbfa4907
Prem Ubhe <99601728+PremUbhe@users.noreply.github.com>
2024-10-19 20:14:45 +0530

    removed comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,42 @@
 // import Image from "next/image";
-// import ProductCard from "./components/ProductCard";
+import ProductCard from "./components/ProductCard";
 
-// type product = {
-//   _id: string;
-//   name: string;
-//   img: string;
-//   price: number;
-//   rating: number;
-//   discount: number;
-//   categories: string;
-//   brand: string;
-//   stock: number;
-//   description: string;
-// }
+type product = {
+  _id: string;
+  name: string;
+  img: string;
+  price: number;
+  rating: number;
+  discount: number;
+  categories: string;
+  brand: string;
+  stock: number;
+  description: string;
+}
 
 
 const Home = async () => {
 
-  // const productsAPI = await fetch(
-  //   `${process.env.BASE_URL}/api/products`,
-  //   {
-  //     headers: {
-  //       Accept: "application/json",
-  //       method: "GET",
-  //     },
-  //   }
-  // );
+  const productsAPI = await fetch(
+    `${process.env.BASE_URL}/api/products`,
+    {
+      headers: {
+        Accept: "application/json",
+        method: "GET",
+      },
+    }
+  );
 
-  // if (!productsAPI.ok) {
-  //   throw new Error(`API call failed with status ${productsAPI.status}`)
-  // }
-  // const productData = await productsAPI.json();
+  if (!productsAPI.ok) {
+    throw new Error(`API call failed with status ${productsAPI.status}`)
+  }
+  const productData = await productsAPI.json();
 
   return (
     <div className="flex flex-wrap">
-      {/* {productsAPI && productData.data.map((data: product, index: string) => {
+      {productsAPI && productData.data.map((data: product, index: string) => {
         return <ProductCard params={data} key={index} />;
-      })} */}
+      })}
       <h1>odnsovd</h1>
     </div>
   );
