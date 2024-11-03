bdb8e93ddb99d4eafbda51d70dec1622b68e85a3
Prem Ubhe <99601728+PremUbhe@users.noreply.github.com>
2024-11-03 15:16:59 +0530

    try to remove error

--- app/(webside)/@navbar/page.tsx
+++ app/(webside)/@navbar/page.tsx
@@ -3,11 +3,18 @@ import NavLinks from "@/components/NavLinks";
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 p-4 bg-white border-b border-primary-light shadow z-50">
-      <div className="container flex items-center justify-end">
-        <div className="flex gap-10">
-          <NavLinks />
-        </div>
+    <nav className="sticky top-0 py-2 px-4 bg-white border-b border-primary-light shadow z-50">
+      <div className="flex justify-between">
+          <div className="flex gap-10 items-center">
+            <button className='bg-primary text-white py-2 px-7 rounded-lg' type='button'>Browse All Categories</button>
+            <NavLinks />
+          </div>
+          <div className="flex">
+            <div className="flex flex-col">
+              <h3 className='text-primary text-2xl font-bold'>1900 - 888</h3>
+              <p className='text-gray text-sm'>24/7 Support Center</p>
+            </div>
+          </div>
       </div>
     </nav>
   )
