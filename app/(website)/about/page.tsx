829d83ce21fbdf9980bd44baa8909284f6d07421
Prem Ubhe <ubheprem1@gmail.com>
2025-01-10 19:37:55 +0530

    about page responsive

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -61,11 +61,11 @@ const page = () => {
     <>
       {/* Hero section start */}
       <section className='container'>
-        <div className="flex gap-10">
-          <div className="w-6/12">
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-6/12 px-4">
             <Image src={About} className='rounded-lg' alt='about img' placeholder='blur' />
           </div>
-          <div className="w-6/12">
+          <div className="w-full lg:w-6/12 px-4">
             <h1 className='text-4xl text-blue font-bold mb-5'>Welcome to Nest</h1>
             <p className='text-text-secondary text-justify mb-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
             <p className='text-text-secondary text-justify mb-3'>Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta et Ut placerat legendos interpre.Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut ornare lectus. Auctor elit sed vulputate mi sit amet. Commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est laborum.</p>
@@ -94,27 +94,27 @@ const page = () => {
       {/* Provide section end */}
       {/* Performance dection start */}
       <section className='container pb-20'>
-        <div className="flex">
-          <div className="w-7/12">
+        <div className="flex flex-wrap">
+          <div className="w-full xl:w-7/12 lg:w-6/12">
             <Image src={performanceImg} className='w-full h-auto' alt='performance Image'></Image>
           </div>
-          <div className="w-5/12 p-4 ms-5">
+          <div className="w-full xl:w-5/12 lg:w-6/12 p-4 ms-5">
             <h5 className='text-2xl text-gray font-semibold'>Our performance</h5>
-            <h2 className='text-5xl text-blue font-bold mb-5'>Your Partner for e-commerce grocery solution</h2>
+            <h2 className='text-3xl md:text-5xl text-blue font-bold mb-5'>Your Partner for e-commerce grocery solution</h2>
             <p className='text-md text-gray mb-3 text-justify'>Ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto</p>
             <p className='text-md text-gray mb-3 text-justify'>Pitatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia</p>
           </div>
         </div>
-        <div className="flex mt-5">
-          <div className="w-4/12 p-4">
+        <div className="flex flex-wrap mt-5">
+          <div className="w-full md:w-4/12 p-4">
             <h3 className='text-3xl text-blue font-semibold mb-2'>Who we are</h3>
             <p className='text-sm text-gray text-justify'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc mattis enim ut tellus eros donec ac odio orci ultrices in. ellus eros donec ac odio orci ultrices in.</p>
           </div>
-          <div className="w-4/12 p-4">
+          <div className="w-full md:w-4/12 p-4">
             <h3 className='text-3xl text-blue font-semibold mb-2'>Our history</h3>
             <p className='text-sm text-gray text-justify'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc mattis enim ut tellus eros donec ac odio orci ultrices in. ellus eros donec ac odio orci ultrices in.</p>
           </div>
-          <div className="w-4/12 p-4">
+          <div className="w-full md:w-4/12 p-4">
             <h3 className='text-3xl text-blue font-semibold mb-2'>Our mission</h3>
             <p className='text-sm text-gray text-justify'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc mattis enim ut tellus eros donec ac odio orci ultrices in. ellus eros donec ac odio orci ultrices in.</p>
           </div>
